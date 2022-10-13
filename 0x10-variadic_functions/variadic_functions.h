ed0335518108c36b0f4e02e966d52c33724616fe
BetelAddisu <67442890+BetelAddisu@users.noreply.github.com>
2022-10-13 12:25:30 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,28 +1,25 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
-
-#include <stdio.h>
+#ifndef VARIADICFUNCTIONSH
+#define VARIADICFUNCTIONSH
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct p - checker struct
- * @p: possibility
- * @f: Function to be used
- */
-typedef struct p
-{
-  char *p;
-  void (*f)(va_list);
-} checker;
 
-void print_c(va_list);
-void print_i(va_list);
-void print_f(va_list);
-void print_s(va_list);
+int _putchar(char c);
+
+/**
+ * struct funckey - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @f: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
 
 #endif
-
