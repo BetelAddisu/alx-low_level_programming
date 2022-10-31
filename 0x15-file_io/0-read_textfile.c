a1811221f633f60e13bfb6545d0477cf6935297a
BetelAddisu <67442890+BetelAddisu@users.noreply.github.com>
2022-10-31 17:11:52 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,47 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: file to read
+ * @letters: number of letters it should read and print
+ * Return: actual numbers it could read and print
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+	int fd, fd_read, fd_write;
+	char *buff;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDWR);
 	if (fd == -1)
+	{
+		free(buff);
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	}
+	fd_read = read(fd, buff, letters);
+	if (fd_read == -1)
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+	fd_write = write(STDOUT_FILENO, buff, fd_read);
+	if (fd_write == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	if (fd_read != fd_write)
+		return (0);
+	free(buff);
 	close(fd);
-
-	free(buf);
-
-	return (nwr);
+	return (fd_write);
 }
